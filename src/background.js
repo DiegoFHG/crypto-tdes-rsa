459a4f8ecac54175e9926155b4a0e0b56512a50b
Diego Francisco Hernandez Guevara <34254918+DiegoFHG@users.noreply.github.com>
2021-05-27 21:47:22 +0200

    [feature] Removed toolbar.

--- src/background.js
+++ src/background.js
@@ -1,11 +1,8 @@
-import { app, protocol, BrowserWindow, ipcMain, dialog } from 'electron';
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib';
-import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer';
 import crypto from 'crypto';
 import { join } from 'path';
-import { writeFileSync, readFileSync } from 'fs';
-import { parse } from 'js2xmlparser';
-import { Parser } from 'xml2js';
+import { app, protocol, BrowserWindow, ipcMain, Menu } from 'electron';
+import { createProtocol } from 'vue-cli-plugin-electron-builder/lib';
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -28,6 +25,8 @@ async function createWindow() {
       contextIsolation: true,
     },
   });
+
+  if (!process.env.IS_TEST) Menu.setApplicationMenu(null);
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
@@ -67,7 +66,6 @@ app.on('ready', async () => {
       console.error('Vue Devtools failed to install:', e.toString());
     }
   }
-
   createWindow();
 });
 
@@ -87,290 +85,20 @@ if (isDevelopment) {
 }
 
 ipcMain.on('toMain', (e, args) => {
-  if (args.method === 'import-text') {
-    const [filePath] = dialog.showOpenDialogSync(BrowserWindow.fromWebContents(e.sender), {
-      title: 'Importar',
-      properties: ['openFile'],
-      filters: [
-        {
-          name: 'txt', extensions: ['txt'],
-        },
-      ],
-    });
-
-    if (filePath !== undefined) {
-      const data = readFileSync(filePath, 'utf8');
-
-      e.sender.send('fromMain', {
-        method: 'result-import-text',
-        payload: data,
-      });
-    }
-  }
-
-  if (args.method === 'export-text') {
-    const { payload } = args;
-    const filePath = dialog.showSaveDialogSync(BrowserWindow.fromWebContents(e.sender), {
-      title: 'Exportar',
-      defaultPath: 'exportado.txt',
-      filters: [
-        {
-          name: 'txt', extensions: ['txt'],
-        },
-      ],
-    });
-
-    if (filePath !== undefined) {
-      writeFileSync(filePath, payload);
-    }
-  }
-
   if (args.method === 'encrypt-tdes') {
-    try {
-      const { payload: { key, text } } = args;
-      const buffer = Buffer.from(key).toString('hex');
-      const newKey = buffer.padEnd(24, 0x00).slice(0, 24);
+    const { payload: { key, text } } = args;
+    let newKey = parseInt(key, 16);
 
-      const encrypt = crypto.createCipheriv('des-ede3', newKey.toString('hex'), '');
-      encrypt.setAutoPadding(true);
-      const cipher = encrypt.update(text, 'utf8', 'base64');
+    while (newKey.length !== 24) newKey += 0x00;
 
-      e.sender.send('fromMain', {
-        method: 'result-tdes',
-        payload: cipher + encrypt.final('base64'),
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  if (args.method === 'unencrypt-tdes') {
-    try {
-      const { payload: { key, text } } = args;
-      const buffer = Buffer.from(key).toString('hex');
-      const newKey = buffer.padEnd(24, 0x00).slice(0, 24);
-
-      const decrypt = crypto.createDecipheriv('des-ede3', newKey.toString('hex'), '');
-      const uncipher = decrypt.update(text, 'base64', 'utf8');
-
-      e.sender.send('fromMain', {
-        method: 'result-tdes',
-        payload: uncipher + decrypt.final('utf8'),
-      });
-    } catch ({ code }) {
-      console.log(code);
-
-      if (code === 'ERR_OSSL_WRONG_FINAL_BLOCK_LENGTH') {
-        e.sender.send('fromMain', {
-          method: 'show-snack-bar',
-          payload: {
-            code: '01',
-          },
-        });
-      }
-
-      if (code === 'ERR_OSSL_BAD_DECRYPT') {
-        e.sender.send('fromMain', {
-          method: 'show-snack-bar',
-          payload: {
-            code: '03',
-          },
-        });
-      }
-    }
-  }
-
-  if (args.method === 'tdes-export') {
-    const { payload: { key } } = args;
-
-    const filePath = dialog.showSaveDialogSync(BrowserWindow.fromWebContents(e.sender), {
-      title: 'Exportar llave',
-      defaultPath: 'llave-tdes.xml',
-      filters: [
-        {
-          name: 'xml', extensions: ['xml'],
-        },
-      ],
-    });
-
-    if (filePath !== undefined) {
-      const xml = parse('TDES', {
-        clave: Buffer.from(key).toString('base64'),
-      }, {
-        declaration: {
-          include: true,
-          encoding: 'utf-8',
-          version: '1.0',
-        },
-        format: {
-          doublequotes: true,
-          pretty: true,
-        },
-      });
-
-      writeFileSync(filePath, xml);
-    }
-  }
-
-  if (args.method === 'tdes-import') {
-    const [filePath] = dialog.showOpenDialogSync(BrowserWindow.fromWebContents(e.sender), {
-      title: 'Importar llave',
-      properties: ['openFile'],
-      filters: [
-        {
-          name: 'xml', extensions: ['xml'],
-        },
-      ],
-    });
-
-    if (filePath !== undefined) {
-      const parser = new Parser();
-      const data = readFileSync(filePath);
-
-      parser.parseStringPromise(data).then((result) => {
-        const { TDES: { clave } } = result;
-
-        e.sender.send('fromMain', {
-          method: 'result-import-tdes',
-          payload: {
-            key: Buffer.from(clave.toString('base64'), 'base64').toString('utf8'),
-          },
-        });
-      });
-    }
-  }
-
-  if (args.method === 'rsa-generate-keypair') {
-    const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
-      modulusLength: 2048,
-      publicKeyEncoding: {
-        type: 'spki',
-        format: 'der',
-      },
-      privateKeyEncoding: {
-        type: 'pkcs8',
-        format: 'der',
-      },
-    });
+    const encrypt = crypto.createCipheriv('des-ede3', newKey.toString('hex').substr(0, 24), '');
+    encrypt.setAutoPadding(true);
+    const cipher = encrypt.update(text, 'utf8', 'base64');
 
     e.sender.send('fromMain', {
-      method: 'result-rsa-keypair',
-      payload: {
-        publicKey: publicKey.toString('base64'),
-        privateKey: privateKey.toString('base64'),
-      },
-    });
-  }
-
-  if (args.method === 'rsa-encrypt') {
-    try {
-      const { payload: { publicKey, text } } = args;
-      const encrypted = crypto.publicEncrypt({
-        key: `-----BEGIN PUBLIC KEY-----\n${publicKey}\n-----END PUBLIC KEY-----`,
-        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-        oaepHash: 'sha256',
-      }, Buffer.from(text.slice(0, 245)));
-
-      e.sender.send('fromMain', {
-        method: 'result-rsa',
-        payload: encrypted.toString('base64'),
-      });
-    } catch ({ code }) {
-      if (code === 'ERR_OSSL_RSA_DATA_TOO_LARGE_FOR_KEY_SIZE') {
-        e.sender.send('fromMain', {
-          method: 'show-snack-bar',
-          payload: {
-            code: '02',
-          },
-        });
-      }
-    }
-  }
-
-  if (args.method === 'rsa-unencrypt') {
-    try {
-      const { payload: { privateKey, text } } = args;
-      const unencrypted = crypto.privateDecrypt({
-        modulusLength: 2048,
-        key: `-----BEGIN PRIVATE KEY-----\n${privateKey}\n-----END PRIVATE KEY-----`,
-        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-        oaepHash: 'sha256',
-      }, Buffer.from(text, 'base64'));
-
-      e.sender.send('fromMain', {
-        method: 'result-rsa',
-        payload: unencrypted.toString(),
-      });
-    } catch ({ code }) {
-      console.log(code);
-
-      if (code === 'ERR_OSSL_RSA_DATA_LEN_NOT_EQUAL_TO_MOD_LEN') {
-        e.sender.send('fromMain', {
-          method: 'show-snack-bar',
-          payload: {
-            code: '01',
-          },
-        });
-      }
-    }
-  }
-
-  if (args.method === 'rsa-export') {
-    const { payload: { publicKey: clave, privateKey: clavePrivada } } = args;
-    const filePath = dialog.showSaveDialogSync(BrowserWindow.fromWebContents(e.sender), {
-      title: 'Exportar llaves',
-      defaultPath: 'llaves-rsa.xml',
-      filters: [
-        {
-          name: 'xml', extensions: ['xml'],
-        },
-      ],
-    });
-
-    if (filePath !== undefined) {
-      const xml = parse('RSA', {
-        clave,
-        clavePrivada,
-      }, {
-        declaration: {
-          include: true,
-          encoding: 'utf-8',
-          version: '1.0',
-        },
-        format: {
-          doublequotes: true,
-          pretty: true,
-        },
-      });
-
-      writeFileSync(filePath, xml);
-    }
-  }
-
-  if (args.method === 'rsa-import') {
-    const [filePath] = dialog.showOpenDialogSync(BrowserWindow.fromWebContents(e.sender), {
-      title: 'Importar llaves',
-      properties: ['openFile'],
-      filters: [
-        {
-          name: 'xml', extensions: ['xml'],
-        },
-      ],
-    });
-
-    const parser = new Parser();
-    const data = readFileSync(filePath);
-
-    parser.parseStringPromise(data).then((result) => {
-      const { RSA: { clave: publicKey, clavePrivada: privateKey } } = result;
-
-      e.sender.send('fromMain', {
-        method: 'result-import-rsa',
-        payload: {
-          publicKey,
-          privateKey,
-        },
-      });
+      method: 'result-tdes-encrypt',
+      payload: cipher + encrypt.final('base64'),
+      newKey,
     });
   }
 });
